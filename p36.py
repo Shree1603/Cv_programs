b6badc69ec999f01e913f58cec1850a8e0e3d50e
Shree1603 <112561419+Shree1603@users.noreply.github.com>
2023-08-23 14:51:48 +0530

    Update p36.py

--- p36.py
+++ p36.py
@@ -1,6 +1,6 @@
 import cv2
 watch_cascade = cv2.CascadeClassifier("C://Users//ASHIK C SABU//Desktop//supraaa//Object-Detection-using-OpenCV-master//watch-cascade.xml")
-img = cv2.imread("C://Users//ASHIK C SABU//Desktop//supraaa//watch.jpg")
+img = cv2.imread("C://Users//HP//Downloads//pt.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 watches = watch_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
 for (x, y, w, h) in watches:
