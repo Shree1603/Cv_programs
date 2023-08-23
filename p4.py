4eecf6c391dd9e7046a7513e5dd8c7b39e467510
Shree1603 <112561419+Shree1603@users.noreply.github.com>
2023-08-23 14:53:02 +0530

    Update p4.py

--- p4.py
+++ p4.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 kernel = np.ones((5,5),np.uint8)
 print(kernel)
-path = "C://Users//HP//Downloads//pt.jpg"
+path = "C://Users//HP//Downloads//pt.jpg" 
 img =cv2.imread(path)
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
