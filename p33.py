68fdfcc87054b393bcc487fd7bb4098ff9fdb1da
Shree1603 <112561419+Shree1603@users.noreply.github.com>
2023-08-23 14:51:01 +0530

    Update p33.py

--- p33.py
+++ p33.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-img = cv2.imread("C://Users//ASHIK C SABU//Desktop//supraaa//supra.jpg", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("C://Users//HP//Downloads//pt.jpg", cv2.IMREAD_GRAYSCALE)
 kernel = np.ones((5,5), np.uint8)
 grad = cv2.morphologyEx(img, cv2.MORPH_GRADIENT, kernel)
 cv2.imshow("Original", img)
