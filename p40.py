319a4f8c82c5d40b3b29721cd12e2d87d4734c3a
Shree1603 <112561419+Shree1603@users.noreply.github.com>
2023-08-23 14:53:15 +0530

    Update p40.py

--- p40.py
+++ p40.py
@@ -1,5 +1,5 @@
 import cv2
-img = cv2.imread("C://Users//ASHIK C SABU//Desktop//supraaa//supra1.jpg")
+img = cv2.imread("C://Users//HP//Downloads//pt.jpg")
 x, y = 100, 100
 width, height = 200, 150
 roi = img[y:y+height, x:x+width]
