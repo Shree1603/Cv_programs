fc9b060248f7a4de188b363fdd37c35a7d79782a
Shree1603 <112561419+Shree1603@users.noreply.github.com>
2023-08-23 14:54:14 +0530

    Update p9.py

--- p9.py
+++ p9.py
@@ -1,5 +1,5 @@
 import cv2
-path ="C://Users//ASHIK C SABU//Desktop//supraaa//supra.jpg"
+path ="C://Users//HP//Downloads//pt.jpg"
 src = cv2.imread(path)
 window_name = 'Image'
 image = cv2.rotate(src, cv2.ROTATE_180)
