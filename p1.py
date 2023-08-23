c593bb73f530fa47e1ed255a1b6f562f7edefb4c
Shree1603 <112561419+Shree1603@users.noreply.github.com>
2023-08-23 14:41:56 +0530

    p1.py

--- p1.py
+++ p1.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 kernel = np.ones((5,5),np.uint8)
 print(kernel)
-path = "C://Users//ASHIK C SABU//Desktop//supraaa//supra.jpg"
+path = "C://Users//HP//Downloads//pt.jpg"
 img =cv2.imread(path)
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 desired_width = 800
